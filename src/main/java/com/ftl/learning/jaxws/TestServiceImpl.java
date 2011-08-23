501f7796144ec7837199eedfeefb2b84a52e2192
szilsan <szilsan@gmail.com>
2011-08-23 08:46:56 +0200

    pom.xml repository modification

--- src/main/java/com/ftl/learning/jaxws/TestServiceImpl.java
+++ src/main/java/com/ftl/learning/jaxws/TestServiceImpl.java
@@ -2,14 +2,22 @@ package com.ftl.learning.jaxws;
 
 import javax.jws.WebService;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @WebService(serviceName = "testService", name = "testService")
 public class TestServiceImpl implements TestService {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(TestService.class);
 
 	@Override
 	public String invert(String text) {
 
+		LOGGER.debug("String to invert: " + text);
+
 		// if it is empty
 		if (text == null || text.trim().length() == 0) {
+			LOGGER.debug(text == null ? "The text was null" : "Inverted text: " + text);
 			return text;
 		}
 
@@ -25,17 +33,18 @@ public class TestServiceImpl implements TestService {
 			sb.append(c);
 		}
 
+		LOGGER.debug("Inverted text: " + sb.toString());
 		return sb.toString();
 	}
 
 	@Override
 	public double[] sumProd(double x, double y) {
-		System.out.println("Start service - sumProd");
 		double[] retValue = new double[2];
 
 		retValue[0] = x + y;
 		retValue[1] = x * y;
 
+		LOGGER.debug("Result of sumprod: " + x + ", " + y + ". Sum=" + retValue[0] + " Prod: " + retValue[1]);
 		return retValue;
 	}
 }
